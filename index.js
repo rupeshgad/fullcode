bb03fa176789abde5f6b17da3fbffbdc2d2636a4
mohan <mohanadhikari0010@gmail.com>
2023-09-10 20:56:01 +0545

    add new code

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("java-connection")
+greeting="me"
+console.log(greeting, "ram")
+console.log(greeting, "mohan")
+
+firstname="hari"
+lastname="bahadur rana"
+age="20"
+console.log(firstname,lastname,age)
+console.log(firstname,lastname,"age is",age) 
+fullname=firstname +lastname  
+// camelcase
+console.log("your name is",fullname)
+paidstatus= true
+console.log("paidstatus is",paidstatus)   
+address="kumarigalli" + "putaliaadsk" //string concentation "kumarigalliputalisadak"
+console.log(address)
