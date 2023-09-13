49b5c4b5bed66de548fcadd818f3d4d7e67ebc90
mohan <mohanadhikari0010@gmail.com>
2023-09-13 18:02:54 +0545

    new java code

--- index.js
+++ index.js
@@ -15,3 +15,30 @@ paidstatus= true
 console.log("paidstatus is",paidstatus)   
 address="kumarigalli" + "putaliaadsk" //string concentation "kumarigalliputalisadak"
 console.log(address)
+// let colorname ="red"
+// // let color=colorname
+// console.log("color:",color)
+// let price;
+// console.log("price",price);//undefined
+// let productVariant=null
+
+
+// let coursses="mern,python,qa" index is important and start with zero and to access value,change,add
+// console.log("coursses",coursses)
+let courses=["mern","python","qa"]
+console.log("courses",courses)
+let person=["mohan","rupesh","milan","abishake"]
+console.log("personname:",person)
+console.log("person at oth present ",person[0])
+person[0]="rakesh"
+console.log("update it with name",person[0])
+console.log("person at 0th previous",person[1])
+person[1]="rush"
+console.log("person at 1th afteer",person[1])
+console.log("after update",person)
+let color=["red","blue","red"]
+console.log("colorname:",color)
+let prices=[200,122,234]
+let evennumber=[1,2,3,4,5]
+let userstatus=[true,false,true]
+console.log("userstatus:",userstatus)
